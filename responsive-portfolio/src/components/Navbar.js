de3170c7c88301be1434e90488b900685cfc119e
Carlos Rodriguez <crodriguez3753@gmail.com>
2021-08-28 13:42:52 -0400

    edited styling for page

--- responsive-portfolio/src/components/Navbar.js
+++ responsive-portfolio/src/components/Navbar.js
@@ -31,7 +31,7 @@ window.addEventListener('resize', showButton);
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                         TRVL <i className='fa fa-typo3'/>
                     </Link>
                 <div className='menu-icon' onClick={handleClick}>
